fa1f1fa30df17a8e2b8c3a86cf6790589e9653c0
Pratik K <pratikkaranje03@gmail.com>
2021-06-10 19:33:07 +0530

    tags change

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Softwate Enginner", "Python Architect", "Technology Consultant", "Data Science Enthusiast"],
+        strings: ["Software Engineer", "Python Architect", "Technology Consultant", "Data Science Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Softwate Enginner", "Python Architect", "Technology Consultant", "Data Science Enthusiast"],
+        strings: ["Software Engineer", "Python Architect", "Technology Consultant", "Data Science Enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
